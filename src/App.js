e39ab56e569c14db297ae29291cc0424098e2912
sriramcodes <sriramcodes@gmail.com>
2021-06-19 13:17:15 -0700

    Implemented Add and Delete Feature

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import React, { useState, useEffect } from "react";
 import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
 import Todo from "./Todo.js";
 import db from "./firebase";
+import firebase from 'firebase'
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(["Item 1", "Item 2", "Item 3"]);
   const [input, setInput] = useState("");
   console.log("🎮", input);
 
   useEffect(() => {
-    db.collection("todos").onSnapshot((snapshot) => {
-      setTodos(snapshot.docs.map((doc) => doc.data().todo));
+    db.collection("todos").orderBy('timestamp','desc').onSnapshot((snapshot) => {
+      setTodos(snapshot.docs.map((doc) => ({id: doc.id, todo:doc.data().todo})));
     });
   }, []);
 
   const addTodo = (event) => {
     event.preventDefault();
+
+    db.collection('todos').add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+
     console.log("Im working");
     setTodos([...todos, input]);
     setInput("");
@@ -46,8 +53,8 @@ function App() {
         </Button>
       </form>
       <ul>
-        {todos.map((todo) => (
-          <Todo text={todo} />
+        {todos.map(todo => (
+          <Todo todo={todo} />
         ))}
       </ul>
     </div>
--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,5 @@
 import {
+  
   List,
   ListItem,
   ListItemAvatar,
@@ -6,14 +7,17 @@ import {
 } from "@material-ui/core";
 import React from "react";
 import "./Todo.css";
+import db from './firebase'
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 function Todo(props) {
   return (
     <List className="todo__list">
       <ListItem>
         <ListItemAvatar />
-        <ListItemText primary={props.text} secondary="Deadline" />
+        <ListItemText primary={props.todo.todo} secondary="Deadline" />
       </ListItem>
+      <DeleteForeverIcon onClick={event => db.collection('todos').doc(props.todo.id).delete()} />
     </List>
   );
 }
